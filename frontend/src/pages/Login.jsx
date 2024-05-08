12512a1fd052cca6bca1564ffbb99df0f0ead1f4
Laksh Soni <55737881+lakshsoni21@users.noreply.github.com>
2024-05-08 12:48:52 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
       password: password,
     };
     axios
-      .post("https://boook-store-api.vercel.app/users/login", data)
+      .post("https://boook-store-4lip.onrender.com/users/login", data)
       .then((response) => {
         if (response.status == 200) {
           localStorage.setItem("token", response.data.token);
