30c326164e19b6d4c03a3d114224022298b9145c
Laksh Soni <55737881+lakshsoni21@users.noreply.github.com>
2024-05-09 12:49:30 +0530

    Create Shimmer.jsx

---
+++ frontend/src/components/Shimmer.jsx
@@ -0,0 +1,53 @@
+export const Shimmer = () => {
+    return (
+        <>
+        <div className="maincontainer flex flex-wrap">
+
+            <div className="cardShimmer">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+            <div className="cardShimmer ">
+                <div className="Top"></div>
+                <div className="bottom"></div>
+            </div>
+        </div>
+        </>
+    )
+}
